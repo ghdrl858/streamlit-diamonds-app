1d6b1f33e7bc7f8fe99bc95d1c693610bb9ed64d
ghdrl858 <ghdrl858@gmail.com>
2022-05-30 12:04:25 +0900

    2022-05-30

--- app.py
+++ app.py
@@ -11,28 +11,28 @@ def main() :
 
     # 옵션 메뉴 꾸미기 코드
     with st.sidebar:
-        menu = option_menu('Menu',['Home','Info','Chart', 'ML'], menu_icon='caret-down-fill', icons = ['house-door-fill','bar-chart-line-fill',"bi bi-robot"], default_index=0,
+        menu = option_menu('Menu',['Home','Info','Chart', 'ML'], menu_icon='caret-down-fill', icons = ['bi bi-house-fill','bi bi-info-circle-fill',"bi bi-bar-chart-line-fill", 'bi bi-tools'], default_index=0,
                          styles={
-        "container": {"padding": "5!important", "background-color": "black", },
+        "container": {"padding": "5!important", "background-color": "#05AFF2", },
         "icon": {"color": "white", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#aeacb0"},
-        "nav-link-selected": {"background-color": "#9ba6fa"},
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#49BF5A"},
+        "nav-link-selected": {"background-color": "#05C7F2"},
     })
     if menu == 'Home':
         run_home()
-    elif menu == 'EDA':
+    elif menu == 'Info':
         run_info()
-    elif menu == 'ML':
+    elif menu == 'Chart':
         run_chart()
     elif menu == 'ML':
         run_ml()
    
     # 이미지 구현
-    with st.sidebar :
-        st.write('')
-        st.write('')
-        image = Image.open('image/headphones.png')
-        st.image(image, use_column_width=True)
+    # with st.sidebar :
+    #     st.write('')
+    #     st.write('')
+    #     image = Image.open('image/headphones.png')
+    #     st.image(image, use_column_width=True)
 
 if __name__ == '__main__' :
     main()
